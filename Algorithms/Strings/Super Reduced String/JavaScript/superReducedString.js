7058a897a5ce17cf80ca7be1f744fda90c404ffa
Dakota <bazookaharwell@gmail.com>
2017-04-26 16:08:16 -1000

    Write successful super reduced string algorithm

--- Algorithms/Strings/Super Reduced String/JavaScript/superReducedString.js
+++ Algorithms/Strings/Super Reduced String/JavaScript/superReducedString.js
@@ -1,5 +1,28 @@
 function processData(input) {
-    //Enter your code here
+  let string = input;
+  // Pass over the string until it's considered "clean"
+  // clean being that no more operations were performed on this string
+
+  while (true) {
+    let clean = true;
+
+    for (let i = 0; i < string.length; ++i) {
+      if (string[i] === string[i+1]) {
+        string = string.slice(0, i) + string.slice(i+2);
+        clean = false;
+        --i;
+      }
+    }
+
+    if (clean) {
+      if (!string) {
+        console.log('Empty String');
+      } else {
+        console.log(string);
+      }
+      return;
+    }
+  }
 }
 
 process.stdin.resume();
