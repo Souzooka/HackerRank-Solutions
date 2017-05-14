97d8b0ac6bea5d97d2504e3e31ed1e6fd57c1333
Dakota <bazookaharwell@gmail.com>
2017-05-13 16:22:08 -1000

    Find Merge Point of Two Lists

---
+++ Data Structures/Linked Lists/Find Merge Point of Two Lists/Python 2/mergePoint.py
@@ -0,0 +1,37 @@
+"""
+ Find the node at which both lists merge and return the data of that node.
+ head could be None as well for empty list
+ Node is defined as
+ 
+ class Node(object):
+ 
+   def __init__(self, data=None, next_node=None):
+       self.data = data
+       self.next = next_node
+
+ 
+"""
+
+def FindMergeNode(headA, headB):
+  stack = []
+  currA = headA
+  currB = headB
+  while (currA):
+    stack.append(currA)
+    currA = currA.next
+    
+  while (currB):
+    if stack.count(currB) == 1:
+      return currB.data
+    currB = currB.next
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
